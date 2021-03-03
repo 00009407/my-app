88643d004550f3e8495dc3f68ab48bbf7443ab48
00009407 <wiut00009407@gmail.com>
2021-03-03 15:15:16 +0500

    add homepage

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express =require('express')
 const app=express()
 
+app.set('view engine', 'pug')
+
 app.get('/',(req, res)=>{
-	res.send('Homepage')
+	res.render('home')
 })
 
 app.listen(8000, err=> {
 	if(err) throw err
 
-		console.log('App is running....')
+	console.log('App is running....')
 })
 
