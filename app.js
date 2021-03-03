40c0f8d19d94ec72aee939ae4e9c7a64d94936fb
00009407 <wiut00009407@gmail.com>
2021-03-03 14:31:13 +0500

    init

---
+++ app.js
@@ -0,0 +1,13 @@
+const express =require('express')
+const app=express()
+
+app.get('/',(req, res)=>{
+	res.send('Homepage')
+})
+
+app.listen(8000, err=> {
+	if(err) throw err
+
+		console.log('App is running....')
+})
+
